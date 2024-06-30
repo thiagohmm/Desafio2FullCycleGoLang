6ffcb9f31809a98e9e14196a3dd47184f5ef1cf9
thiagohmm <thiagohmm@gmail.com>
2024-06-29 21:54:19 -0300

    Fucao feita e testada

--- main.go
+++ main.go
@@ -9,12 +9,19 @@ import (
 	"time"
 )
 
-func GetCep(cep string) string {
-	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+type Links struct {
+	Link string
+	Resp string
+}
+
+func GetCep(linkChan chan Links) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 	startTime := time.Now()
-	// Create a new client
-	req, err := http.NewRequestWithContext(ctx, "GET", "https://viacep.com.br/ws/"+cep+"/json/", nil)
+
+	linkStruct := <-linkChan
+	link := linkStruct.Link
+	req, err := http.NewRequestWithContext(ctx, "GET", link, nil)
 	if err != nil {
 
 		if ctx.Err() == context.DeadlineExceeded {
@@ -38,12 +45,40 @@ func GetCep(cep string) string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	return string(body)
+
+	linkChan <- Links{link, string(body)}
 
 }
 
 func main() {
 
-	fmt.Println(GetCep("03142001"))
+	//fmt.Println(GetCep("https://viacep.com.br/ws/03142001/json/"))
+	//fmt.Println(GetCep("https://brasilapi.com.br/api/cep/v1/03142001"))
+	c1 := make(chan Links)
+	c2 := make(chan Links)
+
+	go GetCep(c1)
+	go GetCep(c2)
+
+	link := Links{"https://viacep.com.br/ws/03142001/json/", ""}
+	c1 <- link
+
+	link = Links{"https://brasilapi.com.br/api/cep/v1/03142001", ""}
+	c2 <- link
+
+	select {
+
+	case msg := <-c1: // rabbitmq
+
+		fmt.Printf("Received from ViaCep: %s - %s\n", msg.Link, msg.Resp)
+
+	case msg := <-c2: // kafka
+
+		fmt.Printf("Received from BrasilApi: %s - %s\n", msg.Link, msg.Resp)
+
+	case <-time.After(time.Second * 3):
+		println("timeout")
+
+	}
 
 }
